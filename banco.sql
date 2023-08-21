4d0979fe083c1403f97fce15cae01c21e8721b21
3Amodular <3mod@bglab.local>
2023-08-21 14:23:43 -0300

    banco

---
+++ banco.sql
@@ -0,0 +1,10 @@
+create database BD;
+use BD;
+
+create table LOGIN(
+codigo int primary key auto_increment,
+usuario varchar(20),
+senha varchar(20) 
+);
+
+insert into LOGIN(usuario, senha) values('admin','1234567');
